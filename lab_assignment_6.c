e179a1707dc142d1512b6cdb038de33ec8d359fc
Lu-Minimum <lucasmarrou@gmail.com>
2024-02-23 23:01:27 -0500

    Filled in body of search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,23 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	//Double check for null or invalid lower bound
+	//though the program would have errors long before this would be called
+	if (numbers==NULL||low<0) {
+		return -1;
+	}
+
+	//Check if the lower bound has passed the upper bound
+	if (low<=high) {
+		//Check lowest index
+		if (numbers[low]==value) {
+			return low;
+		}
+		//Recursively check with lower bound moved up
+		return search(numbers,low+1,high,value);
+	}
+
+	//If low is greater than high, then all numbers in bounds have been checked
 	return -1;
 }
 
